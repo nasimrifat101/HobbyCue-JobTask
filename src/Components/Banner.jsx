ebdab67e0609f4294f241d2c5754c5502f28745d
nasimrifat101 <nasimrifat101@gmail.com>
2024-03-31 02:47:07 +0600

    Started working on banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,8 +1,14 @@
+import colors from "../utils/Colors";
 
 const Banner = () => {
     return (
-        <div>
-            Banner
+        <div style={{ backgroundColor: colors.bannerFir, height: "100vh" }}>
+            <div className="mx-5" >
+                <h1 className="fst-italic fs-1">Explore you <span style={{ color: colors.primary }}>hobby</span> or <span style={{ color: colors.secondary }}>passion</span></h1>
+                <p>
+                    
+                </p>
+            </div>
         </div>
     );
 };
