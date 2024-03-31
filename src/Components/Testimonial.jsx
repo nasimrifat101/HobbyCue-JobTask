e63165c8be2f0730d02d239f71a8c44f46a05fbd
nasimrifat101 <nasimrifat101@gmail.com>
2024-03-31 22:07:00 +0600

    Testimonial complete

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -4,15 +4,15 @@ import author from "../assets/Ellipse 12.png"
 const Testimonial = () => {
     return (
         <div className="vh-100 d-flex justify-content-center align-items-center">
-            <div style={{ backgroundColor: '#F7F5F9' }} className="m-5 p-5 container-xxl">
-                <div className="d-flex align-items-center gap-4">
+            <div style={{ backgroundColor: '#F7F5F9' }} className="m-5 p-5">
+                <div className="d-flex align-items-center gap-4 my-5 p-3">
                     <img src="https://i.postimg.cc/0ykGpmYn/quote.png" alt="" />
                     <h3>Testimonial</h3>
                 </div>
-                <p className="mt-3 fs-5">
+                <p className="my-5 fs-5 p-3">
                     In a fast-growing and ever-changing city like Bangalore, it sometimes becomes very difficult to find or connect with like-minded people. Websites like hobbycue.com are a great service that helps me get in touch with, communicate, connect, and exchange ideas with other dancers. It also provides the extra benefit of finding products and services that I can avail of, which I can be assured is going to be of great quality as it comes recommended by people of the hobbycue community. To have discussions, to get visibility, and to be able to safely explore various hobbies and activities in my city, all under one roof, is an excellent idea and I highly recommend it.
                 </p>
-                <div className='mt-5 d-flex justify-content-between align-items-center'>
+                <div className='my-5 d-flex justify-content-between align-items-center p-3'>
                     <img src={audio} alt="Music Player" />
                     <div className='d-flex gap-4 align-items-center'>
                         <img src={author} alt="Author img" />
