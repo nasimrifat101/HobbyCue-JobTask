f00f160b2c989198110d06cd6256e1409baa6879
nasimrifat101 <nasimrifat101@gmail.com>
2024-03-31 16:00:43 +0600

    Margin padding fixxed

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -11,33 +11,33 @@ const Banner = () => {
     };
 
     return (
-        <div style={{ backgroundColor: colors.bannerFir, height: "92vh", display: "flex", alignItems: "center" }}>
+        <div style={{ backgroundColor: colors.bannerFir, height: "100vh", display: "flex", alignItems: "center" }}>
             <div className="mx-5 container" >
                 <div className="row gap-5">
-                    <div className="col-8">
+                    <div className="col-8 me-5">
                         <h1 className="fst-italic" style={{ fontSize: "3.2rem", marginTop: "9rem" }}>Explore you <span style={{ color: colors.primary }}>hobby</span> or <span style={{ color: colors.secondary }}>passion</span></h1>
                         <p className='fs-5 mt-5'>
                             Sign-in to interact with a community of fellow hobbyists and an eco-system of experts, teachers, suppliers, classes, workshops, and places to practice, participate or perform. Your hobby may be about visual or performing arts, sports, games, gardening, model making, cooking, indoor or outdoor activities…
 
                         </p>
 
-                        <p className='fs-5 mt-5' style={{ marginBottom: "5.2rem" }}>
+                        <p className='fs-5 mt-5' style={{ marginBottom: "10.2rem" }}>
                             If you are an expert or a seller, you can Add your Listing and promote yourself, your students, products,  services or events.  Hop on your hobbyhorse and enjoy the ride.
                         </p>
                         <div className="flex ">
                             <img src={bannerImg} alt="Happy figures" className="w-100" />
                         </div>
                     </div>
-                    <div className="col">
-                        <div className="d-flex gap-5">
+                    <div className="col ms-5">
+                        <div className="d-flex gap-5" style={{ marginTop: "9rem" }}>
                             <p
-                                className={`tab ${activeTab === 'signup' ? `border-bottom border-3 border-primary fw-bold fs-3 ` : 'fw-bold fs-3 text-black-50'}`}
+                                className={`tab ${activeTab === 'signup' ? `border-bottom border-3 purple border-primary fw-bold fs-3` : 'fw-bold fs-3 text-black-50'}`}
                                 onClick={() => handleTabChange('signup')}
                             >
                                 Sign in
                             </p>
                             <p
-                                className={`tab ${activeTab === 'login' ? 'border-bottom border-primary border-3 fw-bold  fs-3' : 'fw-bold  fs-3 text-black-50'}`}
+                                className={`tab ${activeTab === 'login' ? 'border-bottom border-primary border-3 fw-bold purple fs-3' : 'fw-bold  fs-3 text-black-50'}`}
                                 onClick={() => handleTabChange('login')}
                             >
                                 Join
